9d3afe7d7dda78f2971092a4c84c5a85a9bd9b9c
Daphney <daphneyalexandre@hotmail.com>
2024-06-26 22:32:51 -0400

    complete

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def __init__(self):
+        self.dada = []
+    def talk(self):
+        print('Hello World!')
+    def walk(self):
+        print('The person is walking.')
+guido = Person()
+guido.talk()
+guido.walk()
 
-    #Instance method definition
-    pass
